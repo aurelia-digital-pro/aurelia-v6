40fcb0b52b7cc8addda82082f0907e632adbd082
fouedsendi185-tech <fouedsendi185@gmail.com>
2026-05-01 21:00:45 +0200

    Delete pages/api directory

--- pages/api/V6/[...path].js
+++
@@ -1,7 +0,0 @@
-import { supabaseAdmin } from '../../../lib/supabaseAdmin'
-
-export default async function handler(req, res) {
-  const { data, error } = await supabaseAdmin.from('documents').select('*').limit(10)
-  if (error) return res.status(500).json({ error: error.message })
-  res.status(200).json({ library: "Aurelia Memory Engine V6", data })
-}
