1569024d24d120370d6cd56ab264e5f852f33baf
aurelia-digital-pro <fouedsendi185@gmail.com>
2026-05-05 00:26:51 -0300

    Create library.js

---
+++ pages/library.js
@@ -0,0 +1,8 @@
+export default function Library() {
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1>Aurelia Library</h1>
+      <p>مرحلة P2 بدأت ✅</p>
+    </div>
+  );
+}
