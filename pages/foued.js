e89ab81727e89f3feab544c499c6147f2b228b5e
aurelia-digital-pro <fouedsendi185@gmail.com>
2026-05-06 19:26:35 -0300

    Update foued.js

--- pages/foued.js
+++ pages/foued.js
@@ -1,14 +1,15 @@
+import MemoryCore from "../components/MemoryCore";
+
 export default function Foued() {
   return (
-    <div style={{
-      background: "black",
-      color: "white",
-      minHeight: "100vh",
-      padding: "40px",
-      fontFamily: "Arial"
-    }}>
-      <h1>FOUED PRIVATE AI</h1>
-      <p>Aurelia Memory Core Activated.</p>
+    <div
+      style={{
+        background: "#000",
+        minHeight: "100vh",
+        padding: "40px",
+      }}
+    >
+      <MemoryCore />
     </div>
-  )
+  );
 }
