f35ad6dee106bd7906c916e70407786cabb22a51
aurelia-digital-pro <fouedsendi185@gmail.com>
2026-05-06 19:32:46 -0300

    Update foued.js

--- pages/foued.js
+++ pages/foued.js
@@ -1,15 +1,41 @@
-import MemoryCore from "../components/MemoryCore";
+import MemoryPanel from "../components/MemoryPanel"
 
 export default function Foued() {
   return (
     <div
       style={{
         background: "#000",
+        color: "white",
         minHeight: "100vh",
-        padding: "40px",
+        padding: "60px",
+        fontFamily: "serif"
       }}
     >
-      <MemoryCore />
+      <h1
+        style={{
+          fontSize: "72px",
+          lineHeight: "0.95"
+        }}
+      >
+        AURELIA
+        <br />
+        MEMORY
+        <br />
+        CORE
+      </h1>
+
+      <p
+        style={{
+          marginTop: "30px",
+          fontSize: "24px",
+          maxWidth: "700px",
+          color: "#ccc"
+        }}
+      >
+        Central intelligence system for knowledge indexing and private AI memory.
+      </p>
+
+      <MemoryPanel />
     </div>
-  );
+  )
 }
