5ee01c6c3b9ba433c4f004d04f9bdf3a61855384
aurelia-digital-pro <fouedsendi185@gmail.com>
2026-05-06 19:49:11 -0300

    Update foued.js

--- pages/foued.js
+++ pages/foued.js
@@ -1,41 +1,51 @@
-import MemoryPanel from "../components/MemoryPanel"
+import Head from 'next/head'
+import MemoryCore from '../components/MemoryCore'
 
-export default function Foued() {
+export default function FouedPage() {
   return (
-    <div
-      style={{
-        background: "#000",
-        color: "white",
-        minHeight: "100vh",
-        padding: "60px",
-        fontFamily: "serif"
-      }}
-    >
-      <h1
+    <>
+      <Head>
+        <title>Aurelia Founder Space</title>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
+
+      <div
         style={{
-          fontSize: "72px",
-          lineHeight: "0.95"
+          minHeight: '100vh',
+          background: '#000',
+          color: 'white',
+          padding: '60px 20px',
+          fontFamily: 'serif'
         }}
       >
-        AURELIA
-        <br />
-        MEMORY
-        <br />
-        CORE
-      </h1>
+        <div
+          style={{
+            maxWidth: '900px',
+            margin: '0 auto'
+          }}
+        >
+          <h1
+            style={{
+              fontSize: '64px',
+              marginBottom: '10px'
+            }}
+          >
+            AURELIA
+          </h1>
 
-      <p
-        style={{
-          marginTop: "30px",
-          fontSize: "24px",
-          maxWidth: "700px",
-          color: "#ccc"
-        }}
-      >
-        Central intelligence system for knowledge indexing and private AI memory.
-      </p>
+          <p
+            style={{
+              color: '#777',
+              marginBottom: '40px',
+              fontSize: '18px'
+            }}
+          >
+            Founder Intelligence Space
+          </p>
 
-      <MemoryPanel />
-    </div>
+          <MemoryCore />
+        </div>
+      </div>
+    </>
   )
 }
